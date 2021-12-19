7a71df284f0196b65f4701dfe59558af15c77986
Kelechi <kelechiuma@gmail.om>
2021-12-19 10:29:44 +0100

    Added jsonwebtoken to the endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // Register
 router.post('/register', async (req, res) => {
@@ -42,9 +43,18 @@ router.post('/login', async (req, res) => {
       return res.status(401).json('Wrong credentials');
     }
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '3d' }
+    );
+
     const { password, ...others } = user._doc;
 
-    return res.status(200).json(others);
+    return res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
